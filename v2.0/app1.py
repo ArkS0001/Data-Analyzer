a1ed7345597b4a94572f32341915a71d4742eb85
ArkS0001 <shubh26002@gmail.com>
2023-09-19 21:07:09 +0530

    your msg

---
+++ v2.0/app1.py
@@ -0,0 +1,34 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Information": ["Blood type", "Pregnancies", "Insulin", "Skin thickness","Blood type","Insulin"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Information", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Medical Data prediction'),
+
+    html.Div(children='''
+        Model 3 diabetes 
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
